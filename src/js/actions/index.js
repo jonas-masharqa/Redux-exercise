d8f38cb15343f0ee0abe954edc5f552113b109d7
jonas <jonas.masharqa@gmail.com>
2020-01-30 23:26:54 +0100

    Adds thunk to getData action.

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -5,9 +5,11 @@ export function addArticle(payload) {
 }
 
 export function getData() {
-  return fetch('https://jsonplaceholder.typicode.com/posts')
+  return function(dispatch) {
+    return fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => response.json())
     .then(json => {
       return { type: 'DATA_LOADED', payload: json}
     })
+  }
 }
