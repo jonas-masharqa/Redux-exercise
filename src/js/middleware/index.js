b1055094dfaaf739d1d7efb8ac8c7d3bdc648393
jonas <jonas.masharqa@gmail.com>
2020-02-01 16:23:29 +0100

    Adds emptyTitle middleware.

--- src/js/middleware/index.js
+++ src/js/middleware/index.js
@@ -1,6 +1,8 @@
 import { ADD_ARTICLE } from '../constants/action-types'
 
-const forbiddenWords = ['spam', 'money']
+const forbiddenWords = ['spam', 'money', 'Spam', 'Money']
+
+let article = ''
 
 export function forbiddenWordsMiddleware({ dispatch }) {
   return function(next) {
@@ -11,6 +13,20 @@ export function forbiddenWordsMiddleware({ dispatch }) {
         )
         if (foundword.length) {
           return dispatch({ type: 'FOUND_BAD_WORD' })
+        }
+      }
+      return next(action)
+    }
+  }
+}
+
+export function emptyTitle({ dispatch }) {
+  return function(next) {
+    return function(action) {
+      if (action.type === ADD_ARTICLE) {
+        article = action.payload.title
+        if (article == '') {
+          return dispatch({ type: 'NO_TITLE_FOUND' })
         }
       }
       return next(action)
--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,13 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from '../reducers/index'
-import { forbiddenWordsMiddleware } from '../middleware'
+import { forbiddenWordsMiddleware, emptyTitle } from '../middleware'
 import thunk from 'redux-thunk'
 
 const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
   rootReducer,
-  storeEnhancers(applyMiddleware(forbiddenWordsMiddleware, thunk))
+  storeEnhancers(applyMiddleware(forbiddenWordsMiddleware, emptyTitle, thunk))
 )
 
 export default store
